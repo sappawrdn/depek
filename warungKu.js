f6fa1b4f227fc1e25a7506151fc48974ed3d41f7
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-11-20 14:38:04 +0700

    editing slider card in warungKu

--- warungKu.js
+++ warungKu.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-});
-
-$(document).ready(function () {
-    $('.carousel').carousel();
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
